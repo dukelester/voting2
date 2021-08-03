839eb473cf4191a890f378fa1c352c3b3f978013
dukelester <dukelester4@gmail.com>
2021-08-03 18:00:40 +0300

    to be done by anaco

--- account/views.py
+++ account/views.py
@@ -6,6 +6,21 @@ from voting.forms import VoterForm
 from django.contrib.auth import login, logout
 # Create your views here.
 
+from django.contrib.auth.models import User
+from rest_framework import authentication, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class Userlists(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        usernames = [user.username for user in User.objects.all()]
+
+        return Response(usernames)
+
 
 def account_login(request):
     if request.user.is_authenticated:
--- e_voting/settings.py
+++ e_voting/settings.py
@@ -42,11 +42,15 @@ INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'voting.apps.VotingConfig',
     'administrator.apps.AdministratorConfig',
+    'rest_framework',
+    'corsheaders',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
